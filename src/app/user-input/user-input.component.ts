37855729c618fa27e5b449b4d89f3b2dd4c3e29b
Ali Samir <ali.samir@taager.com>
2024-10-30 22:21:04 +0300

    initial commit

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,33 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  initialInvestment = signal('0');
+  annualInvestment = signal('0');
+  expectedReturn = signal('0');
+  duration = signal('0');
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.initialInvestment(),
+      annualInvestment: +this.annualInvestment(),
+      expectedReturn: +this.expectedReturn(),
+      duration: +this.duration(),
+    });
+
+    this.initialInvestment.set('0');
+    this.annualInvestment.set('0');
+    this.expectedReturn.set('0');
+    this.duration.set('0');
+  }
+}
